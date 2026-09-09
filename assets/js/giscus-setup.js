ce8225ce5e627d936b2a09a279baee3203e7ee46
RodrigoMSanchez <50014875+RodrigoMSanchez@users.noreply.github.com>
2026-09-09 10:59:47 +0000

    Deploying to gh-pages from @ RodrigoMSanchez/RodrigoMSanchez.github.io@03e127f237566348ae386e63af14032541f43ff3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "RodrigoMSanchez/RodrigoMSanchez.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
